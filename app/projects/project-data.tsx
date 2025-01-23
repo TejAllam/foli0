0f0c3883b41f3873e4f684dee947f68f430b5dc7
Tej <16512061+TejAllam@users.noreply.github.com>
2025-01-23 06:30:22 +0000

    Adding project and Ikigai

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,15 +7,27 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Mithril AI",
+    title: "IDCF",
     year: 2024,
-    description: "Open science AI resarch lab",
-    url: "https://github.com/mithrilai",
+    description: "Internet Data Collection Facility at the Bureau of Labor Statistics",
+    url: "https://idcf.bls.gov",
   },
   {
-    title: "OpenDeepLearning",
+    title: "Data Finder",
+    year: 2023,
+    description: "Data Finder",
+    url: "https://data.bls.gov/dataQuery/",
+  },
+  {
+    title: "Data Viewer",
     year: 2023,
     description: "Open source AI education resources",
-    url: "https://opendeeplearning.xyz/",
+    url: "https://data.bls.gov/dataViewer/",
   },
+  {
+    title: "PDQ Web",
+    year: 2023,
+    description: "Data Query Single Screen",
+    url: "https://data.bls.gov/PDQWeb/",
+  }, 
 ];
