077adfb2b76331008e85897da67ea7b2b077baf4
TejAllam <tejaswi.allam@gmail.com>
2025-01-30 00:24:53 -0800

    Changes to profile page.

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import { socialLinks } from "./config";
 export default function Page() {
   return (
     <section>
-      <a href={socialLinks.twitter} target="_blank">
+      <a href={socialLinks.linkedin} target="_blank">
         <Image
-          src="/profile.png"
+          src="/profile_me.png"
           alt="Profile photo"
-          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 hover:grayscale-0"
           unoptimized
           width={160}
           height={160}
@@ -19,9 +19,13 @@ export default function Page() {
         
       </h1>
       <div className="prose prose-neutral dark:prose-invert">
-        <p>Just my online presense / Portfolio, as few lines as possible!
-More to come .. 
-        </p>
+
+          <p>Greetings, internet wanderer! I'm <strong>Tej</strong>!</p>
+          <p>An engineer constantly in the <em>making</em>, charting the uncharted territories of tomorrow.</p>
+
+          <p>Fueled by ☕ and the dream of a brighter future (and maybe world peace☮, while we're at it), 
+          I'm dedicating my precious time⏳ on this spinning rock🌎 to doing what I love💖: ,<br/> 
+          wrangling 🖥code and keeping up with the latest tech ⚡.</p>
       </div>
     </section>
   );
