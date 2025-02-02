a2fc93cc7f14a9859e56fe986928391c55035017
TejAllam <tejaswi.allam@gmail.com>
2025-02-01 16:03:26 -0800

    Test : Adding missing styles  ?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./public/**/*.svg"],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx}',
+     "./public/**/*.svg"],
   theme: {
     extend: {
+      colors: {
+        gray: {
+          300: '#D1D5DB',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+        },
+      },
       fontFamily: {
         sans: ["var(--font-geist-sans)"],
         mono: ["var(--font-geist-mono)"],
