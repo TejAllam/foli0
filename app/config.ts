6192475f9e2a719840e99043106820751079406e
Tej Allam <Tejaswi.Allam@gmail.com>
2026-03-29 22:01:15 -0700

    chore: update opengraph png.

--- app/config.ts
+++ app/config.ts
@@ -2,16 +2,15 @@ export const metaData = {
   baseUrl: "https://TejAllam.io/",
   title: "Tej Allam",
   name: "Tej Allam",
-  ogImage: "/opengraph-image.png",
   description:
     "Just my lil corner on the innanet.",
 };
 
 export const socialLinks = {
-  bluesky:"https://bsky.app/profile/tejallam.bsky.social",
+  bluesky: "https://bsky.app/profile/tejallam.bsky.social",
   twitter: "https://x.com/TejAllam",
   bsky: "https://TejAllam.bsky.social",
   github: "https://github.com/TejAllam",
   linkedin: "https://www.linkedin.com/in/TejAllam",
-  email: "mailto:Tejaswi.Allam[at]gmail.com",
+  email: "mailto:TejAllam[dot]io[at]gmail[dot]com",
 };
